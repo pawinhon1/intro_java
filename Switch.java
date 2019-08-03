23d5b83add8bcbdcda78c133df984c373a5eabdf
pay <nattakan@cpru.ac.th>
2019-08-03 15:20:08 +0700

    Switch Case

---
+++ Switch.java
@@ -0,0 +1,21 @@
+public class Switch{
+
+    public static void main(String[] args){
+        int num = 3;
+        switch (num){
+            case 1: System.out.println("Case 1 : Hello!");
+            break;
+
+            case 2: System.out.println("Case 2 : Hi!");
+            break;
+
+            case 3: System.out.println("Case 3 : Nice to meet you.");
+            break;
+
+            case 4: System.out.println("Case 4 : Nice to meet you too.");
+            break;
+
+            default : System.out.println("Default Case");
+        }
+    }
+}
