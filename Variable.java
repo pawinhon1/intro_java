83e479c803183567684dad850c87c71cf7af09fe
pay <nattakan@cpru.ac.th>
2019-08-03 14:19:00 +0700

    Variable

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable{
+
+    public static void main(String[] args){
+        int number = 20;
+        char charName = 'P';
+        String text = "Pawin";
+
+        System.out.println("----Data Type----\nNumber : " + number + "\nChar : " + charName + "\nString : " + text);
+    }
+
+}
