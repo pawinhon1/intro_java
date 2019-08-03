6a4d3a491e531ef28caef655c69595686177c6dc
pay <nattakan@cpru.ac.th>
2019-08-03 15:37:06 +0700

    For Loop

---
+++ ForLoop.java
@@ -0,0 +1,17 @@
+public class ForLoop{
+
+    public static void main(String[] args){
+
+        System.out.println("----For Loop ----\n n++");
+
+        for(int n=1; n<=5; n++){
+            System.out.println("Num = "+n + "\n");
+        }
+
+        System.out.println("_________________________\nn--");
+
+        for(int a=5; a>=1; a--){
+            System.out.println("Num = " + a + "\n");
+        }
+    }
+}
