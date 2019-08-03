4edd513eb10feacc3fe3b2157680f44881f3d3a0
pay <nattakan@cpru.ac.th>
2019-08-03 15:55:15 +0700

    DoWhileLoop

---
+++ DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop{
+
+    public static void main(String[] args){
+        int n = 10;
+        do{
+            System.out.println("Num = " + n);
+            n--;
+        }  
+        while(n >= 1);
+            
+    }
+}
